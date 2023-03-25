c425d7607df05c98f6afb89d02851005688b5ac3
Aleksandra8485 <118071817+Aleksandra8485@users.noreply.github.com>
2023-03-25 16:24:20 +0100

    part 9/10

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnEl = document.querySelector(".change-color");
+const colorEl = document.querySelector(".color");
+
+btnEl.addEventListener("click", () => {
+  colorEl.innerHTML = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+});
